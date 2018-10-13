1fb344a25aa7e9e1692e521adddfa1302aa813ac
unknown <pieternella.luttikhuizen@nioz.nl>
2018-10-13 21:04:48 +0200

    added tests

--- djangocase/cities/models.py
+++ djangocase/cities/models.py
@@ -3,9 +3,13 @@ from django.db import models
 class City(models.Model):
     city_abbreviation = models.CharField(max_length=50)
     city_name = models.CharField(max_length=400)
+    def __str__(self):
+        return self.city_name
     
 class Hotel(models.Model):
     city = models.ForeignKey(City, on_delete=models.CASCADE)
     hotel_city = models.CharField(max_length=50)
     hotel_code = models.CharField(max_length=50)
     hotel_name = models.CharField(max_length=400)
+    def __str__(self):
+        return self.hotel_name
--- djangocase/cities/views.py
+++ djangocase/cities/views.py
@@ -8,32 +8,58 @@ from .models import City, Hotel
 
 djangocase_password = os.environ["DJANGOCASE_PASSWORD"]
 
-requests_hotel = requests.get('http://rachel.maykinmedia.nl/djangocase/hotel.csv', auth=('python-demo', djangocase_password))
-requests_city = requests.get('http://rachel.maykinmedia.nl/djangocase/city.csv', auth=('python-demo', djangocase_password))
-
-decoded_content_hotel = requests_hotel.content.decode('utf-8')
-reader_hotel = csv.reader(decoded_content_hotel.splitlines(), delimiter=';')
-hotel_data = list(reader_hotel)
-
-decoded_content_city = requests_city.content.decode('utf-8')
-reader_city = csv.reader(decoded_content_city.splitlines(), delimiter=';')
-cities_data = list(reader_city)
-
-
-# below is part of the data. This can be used in case the http connection doesn't work, otherwise comment it out
-#cities_data = [['AMS', 'Amsterdam'], ['ANT', 'Antwerpen'], ['ATH', 'Athene'], ['BAK', 'Bangkok'], ['BAR', 'Barcelona'], ['BER', 'Berlijn']]
-#hotel_data = [['AMS', 'AMS01', 'Ibis Amsterdam Airport'], ['AMS', 'AMS02', 'Novotel Amsterdam Airport'], ['ANT', 'ANT01', 'Express by Holiday Inn'], ['ANT', 'ANT02', 'Eden'], ['ANT', 'ANT04', 'Astoria']]
-
-
-# index view showing a list of all cities
-def index(request):
+def get_data(source): #api or csv
+    cities_data = []
+    hotel_data=[]
     City.objects.all().delete()
+    if source == 'api':
+        requests_hotel = requests.get('http://rachel.maykinmedia.nl/djangocase/hotel.csv', auth=('python-demo', djangocase_password))
+        requests_city = requests.get('http://rachel.maykinmedia.nl/djangocase/city.csv', auth=('python-demo', djangocase_password))
+
+        decoded_content_hotel = requests_hotel.content.decode('utf-8')
+        reader_hotel = csv.reader(decoded_content_hotel.splitlines(), delimiter=';')
+        hotel_data = list(reader_hotel)
+
+        decoded_content_city = requests_city.content.decode('utf-8')
+        reader_city = csv.reader(decoded_content_city.splitlines(), delimiter=';')
+        cities_data = list(reader_city)
+    elif source == 'csv':
+        with open('cities/data/city.csv') as csvfile:
+            reader_city = csv.reader(csvfile, delimiter=';')
+            cities_data = list(reader_city)
+        csvfile.close()
+        with open('cities/data/hotel.csv') as csvfile:
+            reader_hotel = csv.reader(csvfile, delimiter=';')
+            hotel_data = list(reader_hotel)
+        csvfile.close()
+        #cities_data = [['AMS', 'Amsterdam'], ['ANT', 'Antwerpen'], ['ATH', 'Athene'], ['BAK', 'Bangkok'], ['BAR', 'Barcelona'], ['BER', 'Berlijn']]
+        #hotel_data = [['AMS', 'AMS01', 'Ibis Amsterdam Airport'], ['AMS', 'AMS02', 'Novotel Amsterdam Airport'], ['ANT', 'ANT01', 'Express by Holiday Inn'], ['ANT', 'ANT02', 'Eden'], ['ANT', 'ANT04', 'Astoria']]
+
+    # add city data to database
     for city in cities_data:
         new_city = City()
         new_city.city_abbreviation = city[0]
         new_city.city_name = city[1]
         new_city.save()
+        # add hotel data to database
+        hotel_data_here = [item for item in hotel_data if item[0] == city[0]]
+        for hotel in hotel_data_here:
+            # check if the hotel exists in the city already
+            # note that there are DUPLICATE hotel names, but they have unique hotel codes
+            # check = my_city.hotel_set.filter(hotel_code = hotel[1], hotel_name = hotel[2])
+            # swap the line above for the one below if you want NO DUPLICATES
+            check = new_city.hotel_set.filter(hotel_name = hotel[2])
+            # if it does not exist yet, add it to the database
+            if len(check) == 0:
+                new_hotel = Hotel(hotel_city = hotel[0], hotel_code = hotel[1], hotel_name = hotel[2], city=new_city)
+                new_hotel.save()
+    
+                
+# index view showing a list of all cities
+def index(request):
+    get_data('api')
     cities = City.objects.all()
+    # show error message if no cities are in the database
     if len(cities) == 0:
         return HttpResponse("No cities are available.")
     else:
@@ -42,23 +68,9 @@ def index(request):
     
 # city view showing all hotels for a city
 def city(request, city_id):
-    Hotel.objects.all().delete()
     # try and see if the city in the url exists
     try:
         my_city = City.objects.get(pk = city_id)
-        # add new hotel data to database
-        for hotel in hotel_data:
-            # only attempt to add a hotel to database if it is situated in the city that was selected
-            if my_city.city_abbreviation == hotel[0]: 
-                # check if the hotel exists in the city already
-                # note that there are DUPLICATE hotel names, but they have unique hotel codes
-                # check = my_city.hotel_set.filter(hotel_code = hotel[1], hotel_name = hotel[2])
-                # swap the line above for the one below if you want NO DUPLICATES
-                check = my_city.hotel_set.filter(hotel_name = hotel[2])
-                # if it does not exist yet, add it to the database
-                if len(check) == 0:
-                    new_hotel = Hotel(hotel_city = hotel[0], hotel_code = hotel[1], hotel_name = hotel[2], city=my_city)
-                    new_hotel.save()
     except (KeyError, City.DoesNotExist):
         return HttpResponse("That city does not exist.")
     return render(request, 'cities/city.html', {'city': my_city})  
