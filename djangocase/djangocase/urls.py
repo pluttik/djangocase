73d5883ee4ce9e6ecbeffcb823bc8e28ffdf93d8
unknown <pieternella.luttikhuizen@nioz.nl>
2018-10-29 09:41:39 +0100

    added root redirect

--- djangocase/djangocase/urls.py
+++ djangocase/djangocase/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from .views import redirect_view
+
 urlpatterns = [
+    path('', redirect_view),
     path('admin/', admin.site.urls),
     path('cities/', include('cities.urls')),
 ]
